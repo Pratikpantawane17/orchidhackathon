30340745f1268120e4696671c6827ba2d4737e83
sanskar sontakke <sanskarsontakke06@gmail.com>
2025-04-13 01:04:28 +0530

    job

---
+++ register_shopkeeper.php
@@ -0,0 +1,20 @@
+<?php
+include 'db.php';
+
+$name      = $_POST['name'];
+$email     = $_POST['email'];
+$phone     = $_POST['phone'];
+$shop_name = $_POST['shop_name'];
+$password  = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO shopkeepers (name, email, phone, shop_name, password)
+        VALUES ('$name', '$email', '$phone', '$shop_name', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Shopkeeper registered successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ register_worker.php
@@ -0,0 +1,20 @@
+<?php
+include 'db.php';
+
+$name     = $_POST['name'];
+$email    = $_POST['email'];
+$phone    = $_POST['phone'];
+$skillset = $_POST['skillset'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO workers (name, email, phone, skillset, password)
+        VALUES ('$name', '$email', '$phone', '$skillset', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Worker registered successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
